004e2941f27afa333b5c4469c3ab0e49d6d446c7
pranabacharya <pacharya808@gmail.com>
2020-07-08 23:17:54 +0530

    new repo

---
+++ js/channel.js
@@ -0,0 +1,21 @@
+document.getElementById("time").innerHTML = Date();
+
+
+function submit() {
+    var name1 = document.getElementById("name1st").value;
+    var name2 = document.getElementById("name2nd").value;
+    var fullname = name1 + " " + name2;
+
+    if (name1 == "" || name2 == "") {
+        alert("Name cannot be Blank");
+    } else if (!isNaN(name1) || !isNaN(name2)) {
+        alert("Name cannot be number")
+    } else {
+        document.getElementById("show_name").innerHTML = ("Hey " + "<br>" + fullname);
+    }
+}
+function reset() {
+    document.getElementById("name1st").value = "";
+    document.getElementById("name2nd").value = "";
+    document.getElementById("show_name").innerHTML = "";
+}
